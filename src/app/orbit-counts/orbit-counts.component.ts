c15fe691ebf7874ae884da894df269669535ed59
Lakshmi <lakshmi.swathi24@gmail.com>
2021-04-13 23:57:51 -0500

    added bonus mission

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input } from '@angular/core';
+import { Satellite } from '../satellite';
 
 @Component({
   selector: 'app-orbit-counts',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orbit-counts.component.css']
 })
 export class OrbitCountsComponent implements OnInit {
-
+  @Input() satellites: Satellite[];
+  types:string[] = [
+    "Space Debris",
+    "Communication",
+    "Probe",
+    "Positioning",
+    "Space Station",
+    "Telescope"
+  ];
+  //countarray: number[] =[0,0,0,0,0,0];
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  satelliteCount(satellites: Satellite[],type:string): number{
+    let count =0;
+    for(let i=0;i<satellites.length;i++){
+      if(satellites[i].type.toLowerCase()===type.toLowerCase()){
+        count++;
+      }
+  }
+  return count;
   }
 
 }
